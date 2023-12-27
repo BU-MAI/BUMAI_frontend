fe89ead6b0f86de9bb1f4ed8f8fb3c00cc4943c1
E-jiwoo <livei07@naver.com>
2023-12-27 22:15:09 +0900

    add : Header 생성

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import * as S from "./style";
+
+const Header = () => {
+  return (
+    <>
+      <S.Container>
+        <S.Logo>부마아이</S.Logo>
+        <S.Font>
+          <S.Login>로그인</S.Login>
+          <S.Signup>회원가입</S.Signup>
+        </S.Font>
+      </S.Container>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 4em;
+  background: #fff;
+  box-shadow: 0rem 0.4rem 0.2rem 0rem rgba(0, 0, 0, 0.05);
+`;
+
+export const Logo = styled.div`
+  position: absolute;
+  left: 7em;
+  top: 0.6em;
+  width: 14.5em;
+  height: 3.8em;
+  flex-shrink: 0;
+  color: #fc8cb4;
+  font-family: Pretendard;
+  font-size: 1.5em;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  user-select: none;
+`;
+
+export const Login = styled.div`
+  position: relative;
+  top: 0.62em;
+`;
+export const Signup = styled.div`
+  position: relative;
+  left: 5em;
+  bottom: 0.7em;
+`;
+
+export const Font = styled.div`
+  position: absolute;
+  top: 0.6em;
+  left: 77em;
+  width: 64.135px;
+  height: 23px;
+  flex-shrink: 0;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 1em;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  cursor: pointer;
+`;
