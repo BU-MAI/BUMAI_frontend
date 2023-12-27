a88a92b65da1efdea8df2c31f528a624c64bacab
kimsiyeon0223 <ksyeon0223@gmail.com>
2023-12-28 08:50:19 +0900

    feat : question1 컴포넌트 퍼블리싱

---
+++ src/pages/Question1/style.js
@@ -0,0 +1,122 @@
+import styled from "styled-components";
+
+export const Question1Layout = styled.div`
+  max-width: 100%;
+  display: flex;
+  padding: 7% 15% 0 15%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Question1Title = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+export const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const QusetionContainer = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const Question = styled.div`
+  border: 2px solid #fc8cb4;
+  padding: 1%;
+  width: 100%;
+  text-align: center;
+`;
+
+export const LeftQuestion = styled.div`
+  width: 30%;
+  margin-right: auto;
+`;
+
+export const RightQuestion = styled.div`
+  width: 30%;
+  margin-left: auto;
+`;
+
+export const QusetionBtn = styled.div`
+  width: 100%;
+  display: flex;
+  padding-top: 5%;
+  justify-content: center;
+  gap: 9%;
+`;
+
+export const Btn5 = styled.button`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  background-color: white;
+  border: 2px solid #fc8cb4;
+  background-color: ${(props) => (props.isSelected ? "#fc8cb4" : "white")};
+`;
+export const Btn4 = styled.button`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  background-color: white;
+  border: 2px solid #fc8cb4;
+  background-color: ${(props) => (props.isSelected ? "#fc8cb4" : "white")};
+`;
+export const Btn3 = styled.button`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  background-color: white;
+  border: 2px solid #fc8cb4;
+  background-color: ${(props) => (props.isSelected ? "#fc8cb4" : "white")};
+`;
+export const Btn2 = styled.button`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  background-color: white;
+  border: 2px solid #fc8cb4;
+  background-color: ${(props) => (props.isSelected ? "#fc8cb4" : "white")};
+`;
+export const Btn1 = styled.button`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  border: 2px solid #fc8cb4;
+  background-color: ${(props) => (props.isSelected ? "#fc8cb4" : "white")};
+`;
+
+export const Q = styled.p`
+  line-height: 5px;
+`;
+
+export const NextBtn = styled.button`
+  width: 12%;
+  height: 8vh;
+  background-color: #fc8cb4;
+  border: none;
+  color: white;
+  border-radius: 10px;
+`;
+
+export const BackBtn = styled.button`
+  width: 12%;
+  height: 8vh;
+  background-color: #fc8cb4;
+  border: none;
+  color: white;
+  border-radius: 10px;
+`;
