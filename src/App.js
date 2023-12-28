021786567ca30ffb2cb4e177783cef3ce2d659ba
kimsiyeon0223 <ksyeon0223@gmail.com>
2023-12-28 10:00:24 +0900

    chore : 경로 설정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./pages/Main/index";
 import Signup from "./components/Signup/index";
 import Login from "./components/Login/index";
 import Question1 from "./pages/Question1/index";
+import ResultPython from "./pages/ResultPython";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/Question1" element={<Question1 />} />
+          <Route path="/ResultPython" element={<ResultPython />} />
         </Routes>
       </BrowserRouter>
     </>
